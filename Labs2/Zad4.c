f02492a43cb94ad450a1dbee8d79ab90cf1fcfe9
Mateusz Martyna <mateuszmartyna44@gmail.com>
2021-11-03 23:32:29 +0100

    Poprawiona wersja

--- Labs2/Zad4.c
+++ Labs2/Zad4.c
@@ -7,16 +7,16 @@ float x = 4.0;
 int y = 15;
 int liczba1 = 140;
 int liczba2 = 5;
-int wynik = liczba1/liczba2;
-float wynik_e = (float)(liczba1/x);
-float wynik_f = (float)liczba1/(float)liczba2;
+float wynik = liczba1/liczba2;
+float wynik_e = (float)liczba1/x;
+int wynik_f = liczba1/liczba2;
 
 printf("Float na int: %d\n", (int)x);
 printf("Int na float: %f\n", (float)y);
-printf("Wynik dzielenia dwoch intow: %d\n", wynik);
-printf("Wynik dzielenia dwoch int jako float: %f\n", (float)wynik);
-printf("Dzielenie int przez float, wynik jako float: %f", wynik_e);
-printf("Dzielenie dwoch int, wynik jako float: %f", wynik_f);
+printf("Wynik dzielenia dwoch intow: %f\n", wynik);
+printf("Wynik dzielenia dwoch int jako float: %f\n", wynik);
+printf("Dzielenie int przez float, wynik jako float: %f\n", wynik_e);
+printf("Dzielenie dwoch int, wynik jako float: %f", (float)wynik_f);
 
 return 0;
 }
