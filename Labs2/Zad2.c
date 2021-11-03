96382bbd2123a9b771f193b5366aa809a78716ce
Mateusz Martyna <mateuszmartyna44@gmail.com>
2021-11-03 18:13:06 +0100

    Skonczone zadanie

--- Labs2/Zad2.c
+++ Labs2/Zad2.c
@@ -7,17 +7,18 @@ unsigned int liczba_1 = 15;
 unsigned int liczba_2 = 3;
 long int liczba_3 = 12;
 long int liczba_4 = 4;
-float liczba_5 = 6;
-float liczba_6 = 2;
+float liczba_5 = 6.3;
+float liczba_6 = 2.1;
 char znak1 = 10;
 char znak2 = 2;
 
-printf("%d + %d = %d\n", liczba_1, liczba_2, liczba_1+liczba_2);
-printf("%d - %d = %d\n", liczba_1, liczba_2, liczba_1-liczba_2);
-printf("%d * %d = %d\n", liczba_1, liczba_2, liczba_1*liczba_2);
-printf("%d / %d = %d\n", liczba_1, liczba_2, liczba_1/liczba_2);
-printf("%d  %d = %d", liczba_1, liczba_2, liczba_1%liczba_2);
+printf("Suma = %d Roznica = %d Mnozenie= %d Dzielenie = %d Reszta modulo = %d\n", liczba_1+liczba_2, liczba_1-liczba_2, liczba_1*liczba_2, liczba_1/liczba_2, liczba_1%liczba_2);
+printf("Suma = %d Roznica = %d Mnozenie= %d Dzielenie = %d Reszta modulo = %d\n", liczba_3+liczba_4, liczba_3-liczba_4, liczba_3*liczba_4, liczba_3/liczba_4, liczba_3%liczba_4);
+printf("Suma = %f Roznica = %f Mnozenie= %f Dzielenie = %f \n", liczba_5+liczba_6, liczba_5-liczba_6, liczba_5*liczba_6, liczba_5/liczba_6); // na floatach nie mozna wykonac dzialania modulo
+printf("Suma = %d Roznica = %d Mnozenie= %d Dzielenie = %d Reszta modulo = %d\n", liczba_1+liczba_2, liczba_1-liczba_2, liczba_1*liczba_2, liczba_1/liczba_2, liczba_1%liczba_2);
+printf("Suma = %d Roznica = %d Mnozenie= %d Dzielenie = %d Reszta modulo = %d\n", znak1+znak2, znak1-znak2, znak1*znak2, znak1/znak2, znak1%znak2);
 
+// Inne typy danych to double, short, long, long double, long long
 
 return 0;
 }
