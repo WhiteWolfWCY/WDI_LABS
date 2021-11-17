65c027cc402255992dabfb9f3d4f5361575b41e3
Mateusz Martyna <mateuszmartyna44@gmail.com>
2021-11-17 16:09:00 +0100

    Rozwiazanie sprawdzianu

--- Labs3/sprawdzian1MM.c
+++ Labs3/sprawdzian1MM.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//1. albo 6. Program, który spróbuje wypisać za pomocą pętli "while" kolejne 15 liczb naturalnych mniejszych od Twojego albumu. 
+//W przypadku liczby podzielnej przez 10 nie wyświetlaj nic (wtedy po prostu program wyświetli mniej liczb).
+
 int main(){
 
-printf("Hello World");
+int nr_albumu=80566;
+int counter=80566-15;
+
+while(counter<nr_albumu){
+    if(counter%10!=0){
+        printf("%d \n", counter);
+    }
+    counter++;
+}
 
 return 0;
 }
