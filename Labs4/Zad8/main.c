d559d4d2349975879b31d7f685b201f3922bfea0
Mateusz Martyna <mateuszmartyna44@gmail.com>
2021-12-01 17:08:05 +0100

    Zadanie 8 - wersja poprawiona
    Zgodnie z treścią zamieniłem jedną funkcję, na 3 funkcję z jedną sterującą działaniem

--- Labs4/Zad8/main.c
+++ Labs4/Zad8/main.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int wybor(int a, int operacja){
-    if(operacja==1){
-        if(a<0){
-            return a*(-1);
-        }else{
-            return a;
-        }
-    }else if(operacja==2){
-        return a*a;
+int sterownik(int a, int wybor){
+    if(wybor==1){
+        return modul(a);
+    }else if(wybor==2){
+        return potega(a);
     }
+}
+
+int modul(int a){
+    if(a<0){
+        return a*(-1);
+    }else{
+        return a;
+    }
+}
+
+int potega(int a){
+    return a*a;
 }
 
 int main()
 {
-    printf("%d \n", wybor(5,2));
-    printf("%d \n", wybor(-55,1));
+    printf("%d \n", sterownik(5,2));
+    printf("%d \n", sterownik(-55,1));
     return 0;
 }
