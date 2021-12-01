83654f92895bacf8ea686526dd38e312ddbe890a
Mateusz Martyna <mateuszmartyna44@gmail.com>
2021-12-01 16:16:42 +0100

    Zadanie 2 - pierwsze rozwiazanie

---
+++ Labs4/Zad2/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int mojWiek(int wiek){
+    wiek=19;
+
+    return wiek;
+}
+
+int main()
+{
+    int war=1;
+    printf("%d", mojWiek(war));
+    return 0;
+}
