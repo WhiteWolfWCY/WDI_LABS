0f3226b3d323cd7f569240ba7ecac543b8975580
Mateusz Martyna <mateuszmartyna44@gmail.com>
2021-12-01 16:01:15 +0100

    sprawdzian2MM
    Rozwiązanie sprawdzianu

--- Labs4/Wejsciowka/sprawdzian2MM/main.c
+++ Labs4/Wejsciowka/sprawdzian2MM/main.c
@@ -3,6 +3,22 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int tab[]={2,52,65632,643,123,882,983,0,231};
+    int size=sizeof(tab)/sizeof(tab[0]);
+    int index=0;
+    int ile_parzystych=0;
+
+    do{
+        printf("Index: %d element: %d \n", index, tab[index]);
+
+        if(tab[index]%2==0){
+            ile_parzystych++;
+        }
+
+        index++;
+    }while(index<size);
+
+    printf("Ile parzystych: %d", ile_parzystych);
+
     return 0;
 }
